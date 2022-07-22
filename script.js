86e2ef1cd85d9650a76097348047b7f672428d25
kenschnarrs <knschnarrs@gmail.com>
2022-07-22 00:52:18 -0500

    finished version 1

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
+function shuffle(site, mpw) {
+
+    var news = ""
+
+    var using = site.lenth > mpw.length && site || mpw
+    var other = using == site && mpw || site
+
+    var j = 0
+
+    for (var i = 0; i < using.length; i++){
+
+
+        news += using.charAt(i)
+
+        if (other.length > j){
+            news += other.charAt(j)
+        }
+        else {
+            j = 0
+        }
+
+        j++
+    }
+
+    return news
+  }
 
 function generatePW(){
     var mpw = document.getElementById("mpw").value
     var site = document.getElementById("site").value
+    var cl = document.getElementById("charlim").value
+
+    
     console.log("PASSWORD", mpw)
     console.log("SITE", site)
 
-    return site+mpw
+    site = site.toLowerCase()
+
+
+    var shuffled = shuffle(site, mpw)
+
+    if (cl){
+        return shuffled.substring(0, cl)
+    }
+
+
+    return shuffled
 }
 
 function setPW() {
